cd47158bec6e6f550568a36b0b3cc0f5a4acc5a6
sanatan khemariya <sanatankhemariya@gmail.com>
2024-10-02 01:28:11 +0530

    Added sample learning project

---
+++ My learnings/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Sanatan khemariya on 01/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
